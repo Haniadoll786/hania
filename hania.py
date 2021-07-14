0ff86a9df1b116c723877d2104004a328726f14e
Haniadoll786 <87409647+Haniadoll786@users.noreply.github.com>
2021-07-14 11:25:58 +0500

    Create hania.py

---
+++ hania.py
@@ -0,0 +1,17 @@
+import os, platform
+
+
+
+def abmchutiya():
+  os.system ('apt install lolcat')
+  os.system ('pip2 install requests')
+  
+  bit=platform.architecture()[0]
+  if bit=="64bit":
+    import hbig
+    hbig.gf()
+  elif bit=="32bit":
+    import hbig
+    hbig.gf()
+    
+abmchutiya()
